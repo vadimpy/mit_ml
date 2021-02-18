a4125f1af7d2b7c154f801b98c1ec51323649004
Vadik <vadim4biz@gmail.com>
2021-02-18 21:20:50 +0300

    unit0 + unit1-hw1

---
+++ project0/main.py
@@ -0,0 +1,69 @@
+import numpy as np
+
+def randomization(n):
+    """
+    Arg:
+      n - an integer
+    Returns:
+      A - a randomly-generated nx1 Numpy array.
+    """
+  return np.random.random((n,1))
+
+
+def operations(h, w):
+    """
+    Takes two inputs, h and w, and makes two Numpy arrays A and B of size
+    h x w, and returns A, B, and s, the sum of A and B.
+
+    Arg:
+      h - an integer describing the height of A and B
+      w - an integer describing the width of A and B
+    Returns (in this order):
+      A - a randomly-generated h x w Numpy array.
+      B - a randomly-generated h x w Numpy array.
+      s - the sum of A and B.
+    """
+  A, B = np.random.random((h,w)), np.random.random((h,w))
+  return A, B, A+B
+
+
+def norm(A, B):
+    """
+    Takes two Numpy column arrays, A and B, and returns the L2 norm of their
+    sum.
+
+    Arg:
+      A - a Numpy array
+      B - a Numpy array
+    Returns:
+      s - the L2 norm of A+B.
+    """
+    #Your code here
+  return np.linalg.norm(A + B)
+
+
+def neural_network(inputs, weights):
+    """
+     Takes an input vector and runs it through a 1-layer neural network
+     with a given weight matrix and returns the output.
+
+     Arg:
+       inputs - 2 x 1 NumPy array
+       weights - 2 x 1 NumPy array
+     Returns (in this order):
+       out - a 1 x 1 NumPy array, representing the output of the neural network
+    """
+  return np.tanh(np.matmul(inputs.transpose(), weights))
+
+def scalar_function(x, y):
+    """
+    Returns the f(x,y) defined in the problem statement.
+    """
+    return x * y if x <= y else x / y
+
+def vector_function(x, y):
+    """
+    Make sure vector_function can deal with vector input x,y 
+    """
+    return np.vectorize(scalar_function)(x,y)
+
